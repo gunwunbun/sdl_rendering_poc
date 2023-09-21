5ce09a6bb18e5a447a8dd0d8a37f0b51b4e5815c
gunwunbun <jvanhouts@protonmail.com>
2023-09-21 16:37:39 +0200

    update

--- README.md
+++ README.md
@@ -9,3 +9,29 @@ The implementation can easily be swapped out for something else, in this case SD
 ## Additional information
 - **Coding standards:** [Google C++ Style Guide](https://google.github.io/styleguide/cppguide.html)
 - **C++ version:** 20
+- The only important files are the ones in `/ui_framework`, and `game.h` + `main.cc` in the root directory. The other files are just there for the visualisation/'city building simulation'.
+
+## How it works
+
+**Directory and file Structure**
+- `/ui_framework`
+  - `/ui.h`
+    - Interface containing basic UI functionality such as creating a window, dealing with frames and drawing a rectangle.
+  - `/sdl_ui.h`
+    - SDL implementation of the UI framework.
+
+**Instantiating the UI**
+```c++
+  std::unique_ptr<UI> ui = std::make_unique<SdlUi>(
+    500, 300, "Rendering PoC", true, false);
+```
+
+**Using it to render things**
+```c++
+while (true) {
+  ui_->StartFrame();
+  // Draw stuff, update objects, handle input. For example:
+  // ui_->DrawRectangle(/* args */)
+  ui_->EndFrame();
+}
+```
--- src/game.cc
+++ src/game.cc
@@ -3,32 +3,29 @@
 
 Game::Game(std::unique_ptr<UI> ui)
 : layer_manager_(), ui_ (std::move(ui)), building_manager_() {
-  int amount_per_layer = 15;
-
   building_manager_.SetMaxPosX(ui_->WindowWidth());
 
-  for (int i = 0; i < amount_per_layer * 1.2; i++)
-    building_manager_.AddBuilding(2, rand()%((4)));
+  const int amount_per_layer = 15;
 
-  for (int i = 0; i < amount_per_layer * 0.9; i++)
-    building_manager_.AddBuilding(1, rand()%((4)));
+  for (int layer = 2; layer >= 0; layer--) {
+    int num_buildings = static_cast<int>(amount_per_layer * (layer == 2 ? 1.2 : 0.9));
 
-  for (int i = 0; i < amount_per_layer; i++)
-    building_manager_.AddBuilding(0, rand()%((4)));
+    for (int i = 0; i < num_buildings; i++) {
+      int type = layer;
+      int position = rand() % 4;
+      building_manager_.AddBuilding(type, position);
+    }
+  }
 }
 
 Game::~Game() = default;
 
 void Game::HandleEvents() {
+  // Handle window close event
+  // NOTE: Not part of this PoC, ignore.
   SDL_Event event;
   SDL_PollEvent(&event);
-
-  switch (event.type) {
-    case SDL_QUIT:
-      is_running_ = false;
-    default:
-      break;
-  }
+  if (event.type == SDL_QUIT) is_running_ = false;
 }
 
 void Game::Update() {
@@ -43,7 +40,6 @@ void Game::Render() {
 
     int yPos = kBaseLayerPosY - (kLayerPosYModifier * building.Layer()) - building.Height();
 
-    // Make sure yPos is at least the same as layer 0's Y position
     yPos = std::max(yPos, kBaseLayerPosY - (kLayerPosYModifier * 0) - building.Height());
 
     ui_->DrawRectangle(building.PosX(), yPos, building.Width(), building.Height(),
